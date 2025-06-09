7fb9b76ce0cd188bb9d0dd96cdd9414f94249680
rayprastya <rayhan.prastya@gmail.com>
2025-06-10 03:49:50 +0700

    remove the pg due to not being used for now | feat: store user session into the redis

--- internal/user/repositories/redis/user.go
+++ internal/user/repositories/redis/user.go
@@ -1,1 +1,49 @@
 package redis
+
+import (
+	"context"
+	"encoding/json"
+
+	"github.com/pusrenk/auth-service/internal/user/entities"
+	"github.com/pusrenk/auth-service/pkg/helpers"
+	"github.com/redis/go-redis/v9"
+)
+
+type UserRedisRepository interface {
+	GetUserBySessionID(ctx context.Context, id string) (*entities.User, error)
+	StoreUserSession(ctx context.Context, user *entities.User) error
+}
+
+type userRedisRepository struct {
+	rdb *redis.Client
+}
+
+func NewUserRedisRepository(rdb *redis.Client) UserRedisRepository {
+	return &userRedisRepository{rdb: rdb}
+}
+
+func (r *userRedisRepository) GetUserBySessionID(ctx context.Context, sessionID string) (*entities.User, error) {
+	data, err := r.rdb.Get(ctx, sessionID).Result()
+	if err != nil {
+		return nil, err
+	}
+
+	var user entities.User
+
+	if err := json.Unmarshal([]byte(data), &user); err != nil {
+		return nil, err
+	}
+
+	return &user, nil
+}
+
+func (r *userRedisRepository) StoreUserSession(ctx context.Context, user *entities.User) error {
+	sessionID := helpers.GenerateSessionID()
+	jsonData, err := json.Marshal(user)
+
+	if err != nil {
+		return err
+	}
+
+	return r.rdb.Set(ctx, sessionID, jsonData, helpers.SessionExpiry).Err()
+}
