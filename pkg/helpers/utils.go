6e74aeaade9ed8345f417d82ccf89e3d5193c324
rayprastya <rayhan.prastya@gmail.com>
2025-06-10 03:47:35 +0700

    adding new helpers vars

--- pkg/helpers/utils.go
+++ pkg/helpers/utils.go
@@ -1,6 +1,7 @@
 package helpers
 
 import (
+	"fmt"
 	"time"
 
 	"github.com/google/uuid"
@@ -10,6 +11,13 @@ const (
 	Development = "development"
 	Production  = "production"
 	SessionExpiry = 24 * time.Hour // 24 hours
+	CustomerServiceHost = "localhost"
+	CustomerServicePort = "50051"
+	UserRole = "user"
+)
+
+var (
+	CustomerServiceURL = fmt.Sprintf("%s:%s", CustomerServiceHost, CustomerServicePort)
 )
 
 func GenerateSessionID() string {
